b5cc32d6211c795ec47bef2c23e22e1f138cc16f
Tommaso Dal Sasso <tommaso.dalsasso@gmail.com>
2015-04-20 11:28:21 +0200

    Commit basic functioning version
    Provide a fixed layout, show main widgets and the image of the treemap.

--- web/devel.js
+++ web/devel.js
@@ -4,11 +4,12 @@ define([
     // --- packages used only during development begin here ---
 
     'tide_lib/devel',
+    'shoreline/ShoreLine-Dev',
     'shoreline/ShoreLine-Tests',
     // 'shoreline/ShoreLine-Examples'
-    'amber/legacy/Benchfib',
-    'amber/legacy/Examples',
-    'amber/legacy/IDE'
+    // 'amber/legacy/Benchfib',
+    // 'amber/legacy/Examples',
+    'amber/legacy/IDE',
 
     // --- packages used only during development end here ---
 ], function (amber) {
